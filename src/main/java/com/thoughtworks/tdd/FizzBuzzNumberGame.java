2d2c13b73da5e65ffc3989c67c5f9c827e34da91
sdp <403213485@qq.com>
2019-08-23 16:07:43 +0800

    3,5,7

--- src/main/java/com/thoughtworks/tdd/FizzBuzzNumberGame.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzNumberGame.java
@@ -6,10 +6,16 @@ public class FizzBuzzNumberGame {
 	
 	public String fizzBuzz(int number) {
 		// TODO Auto-generated method stub
-		if(number == 1) {
-		return "1";
-		}else {
+		String strNumber;
+		if (number %3==0) {
 		return "fizz";
+		}else if (number %5==0) {
+		return "Buzz";	
+		}else if (number %7==0) {
+		return "Whizz";
+		}else{
+		return String.valueOf(number);
+			
 		}
 		
 	}
--- src/test/java/com/thoughtworks/tdd/FizzBuzzNumberGameTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzNumberGameTest.java
@@ -37,5 +37,66 @@ public class FizzBuzzNumberGameTest {
 		Assertions.assertEquals("fizz", word);
 	}
 
+	
+	@Test
+	public void should_return5_when_fizzbuzz_given_5() {
+		//given
+		int number =5;
+		FizzBuzzNumberGame fizzBuzzNumberGame= new FizzBuzzNumberGame();
+			
+		
+		//when
+		String word =fizzBuzzNumberGame.fizzBuzz(number);
+		
+		//then
+		
+		Assertions.assertEquals("Buzz", word);
+	}
+	
+	
+	@Test
+	public void should_return7_when_fizzbuzz_given_7() {
+		//given
+		int number =7;
+		FizzBuzzNumberGame fizzBuzzNumberGame= new FizzBuzzNumberGame();
+			
+		
+		//when
+		String word =fizzBuzzNumberGame.fizzBuzz(number);
+		
+		//then
+		Assertions.assertEquals("Whizz", word);
+	}
+	
+	
+	@Test
+	public void should_return14_when_fizzbuzz_given_14() {
+		//given
+		int number =14;
+		FizzBuzzNumberGame fizzBuzzNumberGame= new FizzBuzzNumberGame();
+			
+		
+		//when
+		String word =fizzBuzzNumberGame.fizzBuzz(number);
+		
+		//then
+		Assertions.assertEquals("Whizz", word);
+	}
+	
+	@Test
+	public void should_return14_when_fizzbuzz_given_16() {
+		//given
+		int number =16;
+		FizzBuzzNumberGame fizzBuzzNumberGame= new FizzBuzzNumberGame();
+			
+		
+		//when
+		String word =fizzBuzzNumberGame.fizzBuzz(number);
+		
+		//then
+		Assertions.assertEquals("16", word);
+	}
 
 }
+
+
