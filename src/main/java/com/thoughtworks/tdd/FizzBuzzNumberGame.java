7c7e2e494bd452bef0642986fd0f82cddd7f543f
sdp <403213485@qq.com>
2019-08-23 16:49:07 +0800

    三个任务都完成

--- src/main/java/com/thoughtworks/tdd/FizzBuzzNumberGame.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzNumberGame.java
@@ -7,17 +7,26 @@ public class FizzBuzzNumberGame {
 	public String fizzBuzz(int number) {
 		// TODO Auto-generated method stub
 		String strNumber;
-		if (number %3==0) {
-		return "fizz";
+		
+		if (number %3==0 && number %5==0 && number %7==0) {
+			return "FizzBuzzWhizz";
+		}else if (number %3==0 && number %5==0) {
+			return "FizzBuzz";
+		}else if (number %3==0 && number %7==0) {
+			return "FizzWhizz";
+		}else if (number %5==0 && number %7==0) {
+			return "BuzzWhizz";
+		}else if (number %3==0) {
+			return "Fizz";
 		}else if (number %5==0) {
-		return "Buzz";	
+			return "Buzz";	
 		}else if (number %7==0) {
-		return "Whizz";
+			return "Whizz";
 		}else{
 		return String.valueOf(number);
-			
 		}
-		
-	}
-	
+	}		
 }
+	
+	
+
--- src/test/java/com/thoughtworks/tdd/FizzBuzzNumberGameTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzNumberGameTest.java
@@ -34,7 +34,7 @@ public class FizzBuzzNumberGameTest {
 		
 		//then
 		
-		Assertions.assertEquals("fizz", word);
+		Assertions.assertEquals("Fizz", word);
 	}
 
 	
@@ -96,7 +96,39 @@ public class FizzBuzzNumberGameTest {
 		//then
 		Assertions.assertEquals("16", word);
 	}
-
+	
+	
+	@Test
+	 public void should_return_Result_when_fizzBuzz_given_Params() {
+	  //given
+	  String[] word = new String[]{"1","2","Fizz","4",
+	    "Buzz","Fizz","Whizz","8","Fizz","Buzz",
+	    "11","Fizz","13","Whizz","FizzBuzz","16",
+	    "17","Fizz","19","Buzz","FizzWhizz","22",
+	    "23","Fizz","Buzz","26","Fizz","Whizz",
+	    "29","FizzBuzz","31","32","Fizz","34",
+	    "BuzzWhizz","Fizz","37","38","Fizz",
+	    "Buzz","41","FizzWhizz","43","44","FizzBuzz",
+	    "46","47","Fizz","Whizz","Buzz","Fizz","52",
+	    "53","Fizz","Buzz","Whizz","Fizz","58","59",
+	    "FizzBuzz","61","62","FizzWhizz","64","Buzz",
+	    "Fizz","67","68","Fizz","BuzzWhizz","71",
+	    "Fizz","73","74","FizzBuzz","76","Whizz",
+	    "Fizz","79","Buzz","Fizz","82","83","FizzWhizz",
+	    "Buzz","86","Fizz","88","89","FizzBuzz","Whizz",
+	    "92","Fizz","94","Buzz","Fizz","97","Whizz","Fizz",
+	    "Buzz","101","Fizz","103","104","FizzBuzzWhizz",
+	    "106","107","Fizz","109","Buzz","Fizz","Whizz","113",
+	    "Fizz","Buzz","116","Fizz","118","Whizz","FizzBuzz"};
+	  FizzBuzzNumberGame fizzBuzzNumberGame= new FizzBuzzNumberGame();
+	  
+	  // when
+			 for(int i = 1; i <= 120; i++) {
+		 String  assertWord = fizzBuzzNumberGame.fizzBuzz(i);
+	   // Then
+	   Assertions.assertEquals(word[i-1], assertWord);
+	  }
+	 }
 }
 
 
